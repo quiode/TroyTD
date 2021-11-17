344cba805bcc89139ac9caec490cffd76d9d7b3f
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-17 20:37:24 +0100

    project init

---
+++ desktop/src/com/troytd/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.troytd.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.troytd.game.TroyTD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TroyTD(), config);
+	}
+}
