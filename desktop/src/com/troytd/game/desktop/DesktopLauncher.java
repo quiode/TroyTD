da854244e05be07f77d7ca6c660cc35a1c1e0ff5
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-18 18:32:24 +0100

    new lwjgl version (3)

--- desktop/src/com/troytd/game/desktop/DesktopLauncher.java
+++ desktop/src/com/troytd/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.troytd.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.troytd.game.TroyTD;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TroyTD(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new TroyTD(), config);
 	}
 }
