30decdb6ce2e6bc8925612e516b312dd08dbbef1
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-11-22 11:58:09 +0100

    settings icon

--- core/src/com/troytd/screens/GameScreen.java
+++ core/src/com/troytd/screens/GameScreen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.troytd.game.TroyTD;
 
@@ -16,12 +20,32 @@ public class GameScreen implements Screen {
     // time when this screen was switched to
     private long screenSwitchDelta = -1;
 
+    // settings Icon
+    private ImageButton settingsButton;
+
     public GameScreen(final TroyTD game) {
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
         screenSwitchDelta = System.currentTimeMillis();
+
+        // settings icon
+        settingsButton = new ImageButton(game.skin, "settings");
+        settingsButton.setSize(64, 64);
+        settingsButton.setPosition(Gdx.graphics.getWidth() - settingsButton.getWidth(), Gdx.graphics.getHeight());
+        settingsButton.addListener(new InputListener(){
+            // TODO: make inputListener work
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("GameScreen", "Pressed Image Button");
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("GameScreen", "Pressed Image Button");
+                return true;
+            }
+        });
     }
 
     /**
@@ -42,6 +66,7 @@ public class GameScreen implements Screen {
         ScreenUtils.clear(0, 0, 0, 1);
         game.batch.begin();
         game.font.draw(game.batch, "Hello World", 100, 100);
+        settingsButton.draw(game.batch, 1);
         game.batch.end();
 
         if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.ESCAPE) && screenSwitchDelta < System.currentTimeMillis() - 100) {
