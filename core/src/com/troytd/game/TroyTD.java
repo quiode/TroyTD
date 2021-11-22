3b51a47e82cdad06cdaddbc40922a06ebdda84f4
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-11-22 11:57:53 +0100

    load skin

--- core/src/com/troytd/game/TroyTD.java
+++ core/src/com/troytd/game/TroyTD.java
@@ -1,20 +1,26 @@
 package com.troytd.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.troytd.screens.MainMenuScreen;
 
 public class TroyTD extends Game {
 
     public SpriteBatch batch;
     public BitmapFont font;
+    public Skin skin;
 
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont(); // use libGDX's default Arial font
         font.getData().setScale(2);
         this.setScreen(new MainMenuScreen(this));
+
+        // set skin
+        skin = new Skin(Gdx.files.internal("skins/troytd.json"));
     }
 
     public void render() {
