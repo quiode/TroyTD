2e8b2a4a10861ffcc08eb642ea46c693a9eccfda
Tixfun <clement.lucas-hirtz@kantiwattwil.ch>
2021-11-23 15:50:01 +0100

    dispose

--- core/src/com/troytd/game/TroyTD.java
+++ core/src/com/troytd/game/TroyTD.java
@@ -42,6 +42,7 @@ public class TroyTD extends Game {
         assetManager.finishLoading();
         skin = assetManager.get("skins/troytd.json", Skin.class);
         this.setScreen(new MainMenuScreen(this));
+        dispose();
     }
 
     public void render() {
