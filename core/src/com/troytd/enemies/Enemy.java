99a5ec7159efeced1c60fbf5839c84a078cf6694
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-11-23 08:21:13 +0100

    base classes

---
+++ core/src/com/troytd/enemies/Enemy.java
@@ -0,0 +1,10 @@
+package com.troytd.enemies;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Enemy {
+    protected int damage;
+    protected Texture texture;
+    protected Rectangle form;
+}
---
+++ core/src/com/troytd/towers/Tower.java
@@ -0,0 +1,13 @@
+package com.troytd.towers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Tower {
+    protected Rectangle form;
+    protected int hp = 100;
+    protected Texture texture;
+
+    public Tower() {
+    }
+}
