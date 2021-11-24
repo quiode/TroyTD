8c82a0fc0afc66988b536894321a31481d3976b8
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-24 19:24:34 +0100

    better game objects

--- core/src/com/troytd/maps/Map.java
+++ core/src/com/troytd/maps/Map.java
@@ -2,9 +2,13 @@ package com.troytd.maps;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.CatmullRomSpline;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 
+/**
+ * A Map with a texture, it's path, and the places where towers can be placed
+ */
 public class Map {
     final protected TroyTD game;
 
@@ -13,11 +17,27 @@ public class Map {
     protected CatmullRomSpline<Vector2> path;
     protected Vector2[] dataSet;
 
+    protected Circle[] towerPlaces;
+
+    /**
+     * A Map with a texture, it's path, and the places where towers can be placed on the map
+     *
+     * @param game the game instance
+     */
     public Map(TroyTD game) {
         this.game = game;
     }
 
+    /**
+     * deletes all no longer used stuff
+     */
     public void dispose() {
         texture.dispose();
+    }
+
+    /**
+     * draws the map
+     */
+    public void draw() {
     }
 }
--- core/src/com/troytd/towers/Tower.java
+++ core/src/com/troytd/towers/Tower.java
@@ -1,7 +1,8 @@
 package com.troytd.towers;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 
 public class Tower {
@@ -10,12 +11,14 @@ public class Tower {
     protected final static int speed = 100;
     protected final static int cost = 100;
     protected final static int hp = 100;
+    protected final static int radius = 100;
     protected final TroyTD game;
-    protected Rectangle form;
+    protected final Circle form;
     protected Texture texture;
 
-    public Tower(TroyTD game) {
+    public Tower(TroyTD game, Vector2 position) {
         this.game = game;
+        form = new Circle(position.x, position.y, radius);
     }
 
     public void dispose() {
