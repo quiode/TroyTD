d8c81075c117271e426b881dd630b2f77d02bd68
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-28 21:55:14 +0100

    improved enemy and map classes

--- core/src/com/troytd/enemies/Enemy.java
+++ core/src/com/troytd/enemies/Enemy.java
@@ -2,18 +2,47 @@ package com.troytd.enemies;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 
+/**
+ * an enemy with its texture, health and other related properties
+ */
 public class Enemy {
-    protected final static int hp = 100;
-    protected final static int speed = 100;
-    protected final static int damage = 10;
+    protected final static short hp = 100;
+    protected final static short speed = 100;
+    protected final static short damage = 10;
+    protected final byte line;
     protected final TroyTD game;
     protected Sprite enemySprite;
     protected Rectangle form;
+    protected Vector2 position;
 
-    public Enemy(TroyTD game) {
+    /**
+     * @param line     the line where the enemy is located, 0 is the top line, 1 is the middle line, 2 is the bottom line
+     * @param game     the game instance
+     * @param position the position of the enemy
+     */
+    public Enemy(byte line, final TroyTD game, final Vector2 position) {
+        this.line = line;
         this.game = game;
+        this.position = position;
+    }
+
+    /**
+     * @param line the line where the enemy is located, 0 is the top line, 1 is the middle line, 2 is the bottom line
+     * @param game the game instance
+     */
+    public Enemy(byte line, final TroyTD game) {
+        this(line, game, new Vector2(0, 0));
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
     }
 
     public void dispose() {
--- core/src/com/troytd/maps/Map.java
+++ core/src/com/troytd/maps/Map.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
+import com.troytd.towers.Tower;
 
 /**
  * A Map with a texture, it's path, and the places where towers can be placed
@@ -13,14 +14,26 @@ import com.troytd.game.TroyTD;
 public class Map {
     protected final TroyTD game;
     protected final String pathName;
-    protected final Vector2[] towerPlaces;
     /**
      * the vector by which the map is scaled
      */
-    public Vector2 mapDistortion;
+    protected Vector2 mapDistortion;
+    /**
+     * the places where towers can be placed and the tower
+     */
+    protected towerPlace[] towerPlaces;
+    /**
+     * the points where the path is
+     */
     protected Vector2[] pathPoints;
+    /**
+     * the path for the enemies to follow
+     */
     protected CatmullRomSpline<Vector2> path;
     // Assets
+    /**
+     * the texture of the map
+     */
     protected Sprite mapSprite = null;
 
     /**
@@ -38,17 +51,13 @@ public class Map {
         // set values
         this.game = game;
         this.pathName = texturePath;
-        this.towerPlaces = towerPlaces;
+        this.towerPlaces = new towerPlace[towerPlaces.length];
+        for (int i = 0; i < towerPlaces.length; i++) {
+            this.towerPlaces[i] = new towerPlace(towerPlaces[i], null);
+        }
         this.pathPoints = pathPoints;
     }
 
-    /**
-     * deletes all no longer used stuff
-     */
-    public void dispose() {
-        game.assetManager.unload(pathName);
-        mapSprite.getTexture().dispose();
-    }
 
     /**
      * draws the map
@@ -59,6 +68,10 @@ public class Map {
         }
     }
 
+    /**
+     * function that gets called after the assets are loaded
+     * sets the path and the map distortion
+     */
     public void afterLoad() {
         mapSprite = new Sprite(game.assetManager.get(pathName, Texture.class));
         mapSprite.setSize(game.settingPreference.getInteger("width"), game.settingPreference.getInteger("height"));
@@ -72,5 +85,41 @@ public class Map {
             pathPoint.y *= mapDistortion.y;
         }
         this.path = new CatmullRomSpline<Vector2>(pathPoints, false);
+    }
+
+    /**
+     * @param position the position of the tower
+     * @param tower    the tower to place
+     * @return returns 1 if the tower can be placed, 0 if it can't, and -1 if it can be placed but there is already a tower
+     */
+    public byte placeTower(final Vector2 position, final Tower tower) {
+        for (towerPlace towerPlace : towerPlaces) {
+            if (towerPlace.place.equals(position)) {
+                if (towerPlace.tower == null) {
+                    towerPlace.tower = tower;
+                    return 1;
+                }
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * deletes all no longer used stuff
+     */
+    public void dispose() {
+        game.assetManager.unload(pathName);
+        mapSprite.getTexture().dispose();
+    }
+}
+
+class towerPlace {
+    public final Vector2 place;
+    public Tower tower;
+
+    public towerPlace(final Vector2 place, final Tower tower) {
+        this.place = place;
+        this.tower = tower;
     }
 }
