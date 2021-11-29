d7e589a9b6a020d7968534451d0c93d0326a2a85
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-11-29 11:44:08 +0100

    topHud begin

--- core/src/com/troytd/maps/Map.java
+++ core/src/com/troytd/maps/Map.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
+import com.troytd.hud.topHUD;
 import com.troytd.towers.Tower;
 
 /**
