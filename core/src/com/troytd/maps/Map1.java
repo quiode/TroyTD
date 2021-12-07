9702bdcf19690916dba4847393acbcc75b5ea838
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-12-07 16:14:53 +0100

    short term debug for map

--- core/src/com/troytd/maps/Map1.java
+++ core/src/com/troytd/maps/Map1.java
@@ -1,9 +1,24 @@
 package com.troytd.maps;
 
+import com.badlogic.gdx.math.Vector2;
+import com.troytd.enemies.Enemy;
 import com.troytd.game.TroyTD;
+import com.troytd.towers.Tower;
 
-public class Map1 extends Map{
-    public Map1(final TroyTD game){
-        super(game);
+public class Map1 extends Map {
+    /**
+     * A Map with a texture, it's path, and the places where towers can be placed on the map
+     *
+     * @param game        the game instance
+     * @param texturePath the path to the texture
+     * @param towerPlaces the places where towers can be placed
+     * @param pathPoints  the points that make up the path
+     * @param maxRounds   the amount of rounds needed to win
+     * @param name        the name of the map
+     * @param enemies
+     * @param towers
+     */
+    public Map1(TroyTD game, String texturePath, Vector2[] towerPlaces, Vector2[] pathPoints, byte maxRounds, String name, Class<? extends Enemy>[] enemies, Class<? extends Tower>[] towers) {
+        super(game, texturePath, towerPlaces, pathPoints, maxRounds, name, enemies, towers);
     }
 }
