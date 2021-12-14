84c25bbd6a5a7ed9a7e6314dcd0e01917c6fdabe
Tixfun <clement.lucas-hirtz@kantiwattwil.ch>
2021-12-14 16:20:47 +0100

    spearthrower edited

--- core/src/com/troytd/towers/Spearthrower.java
+++ core/src/com/troytd/towers/Spearthrower.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 
 public class Spearthrower extends Tower{
-    public final static int cost = 20;
-    public final static int damage = 500;
-    public final static int range = 100;
+    public final static int cost = 80;
+    public final static int damage = 40;
+    public final static int range = 160;
     public final static int speed = 100;
     public final static int maxHP = 100;
     public Spearthrower(TroyTD game, Vector2 position, Texture texture, String name, TowerTypes type,
                         Vector2 distortion) {
-        super(game, position, new Texture(""), "Spearthrower", TowerTypes.SINGLE_TARGET, distortion);
+        super(game, position, new Texture("towers/range.jpg"), "Spearthrower", TowerTypes.SINGLE_TARGET, distortion);
     }
 }
