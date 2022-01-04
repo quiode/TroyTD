d6c25cc538ac1f637002b9d1edeab7f4002d7405
Tixfun <clement.lucas-hirtz@kantiwattwil.ch>
2022-01-04 16:18:20 +0100

    enemies und shot

--- core/src/com/troytd/enemies/Poseidon.java
+++ core/src/com/troytd/enemies/Poseidon.java
@@ -4,16 +4,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 import com.troytd.maps.Map;
-import com.troytd.towers.Tower;
 
-public class Archer extends Enemy{
-    public final static short spawnSpeed = 2000;
-    public final static short hp = 50;
+public class Poseidon extends Enemy{    public final static short spawnSpeed = 2000;
+    public final static short hp = 600;
     public final static int speed = 50;
-    public final static short damage = 20;
+    public final static short damage = 50;
     public final static float sizeModifier = 0.1f;
     public final static float atspeed = 0.8f;
-    public final static short range = 160;
+    public final static short range = 40;
 
     /**
      * @param line       the line where the enemy is located, 0 is the top line, 1 is the middle line, 2 is the bottom line
@@ -24,8 +22,8 @@ public class Archer extends Enemy{
      * @param path       the path the enemy will follow, in precalculated points
      * @param map
      */
-    public Archer(byte line, TroyTD game, Vector2 position, Texture texture, Vector2 distortion, Vector2[] path,
-                  Map map) {
+    public Poseidon(byte line, TroyTD game, Vector2 position, Texture texture, Vector2 distortion, Vector2[] path,
+                    Map map) {
         super(line, game, position, new Texture("towers/range.jpg"), distortion, path, map);
     }
 }
--- core/src/com/troytd/enemies/Swordsman.java
+++ core/src/com/troytd/enemies/Swordsman.java
@@ -4,16 +4,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.troytd.game.TroyTD;
 import com.troytd.maps.Map;
-import com.troytd.towers.Tower;
 
-public class Archer extends Enemy{
+public class Swordsman extends Enemy{
     public final static short spawnSpeed = 2000;
-    public final static short hp = 50;
+    public final static short hp = 100;
     public final static int speed = 50;
     public final static short damage = 20;
     public final static float sizeModifier = 0.1f;
-    public final static float atspeed = 0.8f;
-    public final static short range = 160;
+    public final static float atspeed = 0.5f;
+    public final static short range = 20;
 
     /**
      * @param line       the line where the enemy is located, 0 is the top line, 1 is the middle line, 2 is the bottom line
@@ -24,8 +23,8 @@ public class Archer extends Enemy{
      * @param path       the path the enemy will follow, in precalculated points
      * @param map
      */
-    public Archer(byte line, TroyTD game, Vector2 position, Texture texture, Vector2 distortion, Vector2[] path,
-                  Map map) {
+    public Swordsman(byte line, TroyTD game, Vector2 position, Texture texture, Vector2 distortion, Vector2[] path,
+                     Map map) {
         super(line, game, position, new Texture("towers/range.jpg"), distortion, path, map);
     }
 }
