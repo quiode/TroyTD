cf23ad7483846d6a69d9a9500fe8c98f5b039932
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-01-20 14:22:07 +0100

    Revert "particle editor"
    This reverts commit 6622d52addf8ee73b7ad2227f71debba7084eb67.

--- programs/ParticleEditor/ParticleEditor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp gdx.jar:gdx-natives.jar:gdx-backend-lwjgl.jar:gdx-backend-lwjgl-natives.jar:extensions/gdx-tools/gdx-tools.jar com.badlogic.gdx.tools.particleeditor.ParticleEditor
