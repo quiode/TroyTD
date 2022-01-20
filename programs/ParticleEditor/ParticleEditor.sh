6622d52addf8ee73b7ad2227f71debba7084eb67
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-01-20 13:56:42 +0100

    particle editor

---
+++ programs/ParticleEditor/ParticleEditor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp gdx.jar:gdx-natives.jar:gdx-backend-lwjgl.jar:gdx-backend-lwjgl-natives.jar:extensions/gdx-tools/gdx-tools.jar com.badlogic.gdx.tools.particleeditor.ParticleEditor
